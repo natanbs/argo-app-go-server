795dcc0da9262a5c30cad4de9f73742296dc1bff
Natan Bensoussan <arielbs@gmail.com>
2026-06-19 13:57:15 +0300

    health check

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "ok"}`)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello from Go server on port 8090!\n")
 	})
+	http.HandleFunc("/health", healthHandler)
 	log.Println("Go server listening on :8090")
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
